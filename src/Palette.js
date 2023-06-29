c5a4d62277b497bf638771016e67c64509b8f39b
ttiplwviams01 <ttiplwviams01@gmail.com>
2023-06-29 16:44:23 +0530

    added snackbar and styled select

--- src/Palette.js
+++ src/Palette.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
+import Snackbar from "@mui/material/Snackbar";
+import CloseIcon from "@mui/icons-material/Close";
+import IconButton from "@mui/material/IconButton";
 import "./Palette.css";
 
 const Palette = ({ colors }) => {
-  const [data, setData] = useState({ level: 500, format: "hex" });
+  const [data, setData] = useState({ level: 500, format: "hex", open: false });
   //   const [level, setLevel] = useState(500);
 
   const handleLevel = (level) => {
@@ -12,7 +15,15 @@ const Palette = ({ colors }) => {
   };
 
   const handleFormat = (e) => {
-    setData({ ...data, format: e.target.value });
+    setData({ ...data, format: e.target.value, open: true });
+  };
+
+  const handleOpen = () => {
+    setData({ ...data, open: true });
+  };
+
+  const handleClose = () => {
+    setData({ ...data, open: false });
   };
 
   return (
@@ -29,6 +40,26 @@ const Palette = ({ colors }) => {
         ))}
       </div>
       {/* Footer goes here */}
+      <Snackbar
+        open={data.open}
+        onClose={handleClose}
+        autoHideDuration={1000}
+        anchorOrigin={{ horizontal: "left", vertical: "bottom" }}
+        message={<span id="message-id">Format Changed To : {data.format}</span>}
+        ContentProps={{
+          "aria-describedby": "message-id",
+        }}
+        action={[
+          <IconButton
+            onClick={handleClose}
+            color="#fff"
+            key="close"
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>,
+        ]}
+      />
     </div>
   );
 };

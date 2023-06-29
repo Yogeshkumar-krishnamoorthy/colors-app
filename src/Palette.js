ec9ce65d77e7969b2e4a5962f38c6d33d7d83e68
ttiplwviams01 <ttiplwviams01@gmail.com>
2023-06-29 10:24:13 +0530

    colors app created and seedColors and palette components added

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Palette = () => {
+    return (
+        <div className='Palette'>
+            {/* Navbar goes here */}
+           <div className='Palette-Colors'>
+            {/* color boxes goes here */}
+           </div>
+            {/* Footer goes here */}
+        </div>
+    );
+}
+
+export default Palette;

db29993aafa2e49c50868486fbf286799da4808b
ttiplwviams01 <ttiplwviams01@gmail.com>
2023-06-29 13:03:10 +0530

    colorHelper added to generate colors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { getPalette } from "./ColorHelpers";
 import "./App.css";
 
 function App() {
+  console.log(getPalette(seedColors[4]));
   return (
     <div className="App">
       <Palette {...seedColors[5]} />

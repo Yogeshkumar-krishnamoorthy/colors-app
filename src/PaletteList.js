a04a9551e029b55556547fe66c700180220058c1
ttiplwviams01 <ttiplwviams01@gmail.com>
2023-06-30 14:54:09 +0530

    adding styles in palette list component

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -2,30 +2,56 @@ import React from "react";
 import MiniPalette from "./MiniPalette";
 import { styled } from "@mui/system";
 
+const PalettesList = styled("div", {
+  name: "palettes",
+  slot: "list",
+})({
+  minHeight: "100vh",
+  padding: 20,
+  backgroundColor: "blue",
+});
+
+const PalettesListHeader = styled("h1", {
+  name: "palettes-list",
+  slot: "header",
+})({
+  maxWidth: 900,
+  color: "#000",
+  fontSize: "2.5rem",
+  fontWeight: "600",
+  textAlign: "center",
+  padding: 10,
+  margin: "0 auto 1rem auto",
+  display: "flex",
+  justifyContent: "space-between",
+});
+
 const PalettesContainer = styled("div", {
   name: "palettes",
   slot: "container",
 })({
+  maxWidth: 900,
+  margin: "0 auto",
   display: "flex",
   justifyContent: "center",
   flexWrap: "wrap",
   gap: 10,
   "& > *": {
-    width: "280px",
-    padding: "10px",
+    width: 280,
+    padding: 10,
   },
 });
 
 const PaletteList = ({ palette }) => {
   return (
-    <div className="PaletteList">
-      <h1 style={{ textAlign: "center" }}>React Palette</h1>
+    <PalettesList>
+      <PalettesListHeader>React Palette</PalettesListHeader>
       <PalettesContainer>
         {palette.map((p) => (
           <MiniPalette {...p} />
         ))}
       </PalettesContainer>
-    </div>
+    </PalettesList>
   );
 };
 

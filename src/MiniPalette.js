13ca4be2d5a52561e784c14d491a4856724e8b0a
ttiplwviams01 <ttiplwviams01@gmail.com>
2023-06-30 13:47:57 +0530

    adding styles in mini palette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,25 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { styled } from "@mui/system";
 
 const StyledMiniPalette = styled("div", {
   name: "mini",
   slot: "palette",
+})({});
+
+const StyledLink = styled(Link, {
+  name: "mini-palette",
+  slot: "link",
 })({
-  backgroundColor: "purple",
-  border: "3px solid teal",
-  transition: "all 1s ease-in-out",
-  "&:hover": {
-    backgroundColor: "blue",
-    borderColor: "blue",
-  },
-  "& h2": {
-    color: "white",
-  },
+  width: "100%",
+  height: "100%",
+  textDecoration: "none",
+  color: "#000",
 });
-const MiniPalette = () => {
+
+const PaletteTitle = styled("div", {
+  name: "mini-palette",
+  slot: "title",
+})({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  columnGap: 10,
+});
+
+const MiniPalette = ({ paletteName, id, emoji, colors }) => {
   return (
     <StyledMiniPalette>
-      <h2>Mini Palette</h2>
+      <StyledLink to={`/palette/${id}`}>
+        <div>
+          {colors.map((c) => (
+            <div style={{ backgroundColor: c.color }} />
+          ))}
+        </div>
+        <PaletteTitle>
+          <span>{paletteName}</span>
+          <span>{emoji}</span>
+        </PaletteTitle>
+      </StyledLink>
     </StyledMiniPalette>
   );
 };
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,24 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import MiniPalette from "./MiniPalette";
+import { styled } from "@mui/system";
+
+const PalettesContainer = styled("div", {
+  name: "palettes",
+  slot: "container",
+})({
+  display: "flex",
+  justifyContent: "center",
+  flexWrap: "wrap",
+  gap: 10,
+  "& > *": {
+    width: "280px",
+    padding: "10px",
+  },
+});
 
 const PaletteList = ({ palette }) => {
   return (
     <div className="PaletteList">
-      <MiniPalette />
       <h1 style={{ textAlign: "center" }}>React Palette</h1>
-      <div
-        style={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}
-      >
+      <PalettesContainer>
         {palette.map((p) => (
-          <Link
-            style={{ width: "30%", textAlign: "center" }}
-            to={`/palette/${p.id}`}
-          >
-            {p.paletteName}
-          </Link>
+          <MiniPalette {...p} />
         ))}
-      </div>
+      </PalettesContainer>
     </div>
   );
 };
